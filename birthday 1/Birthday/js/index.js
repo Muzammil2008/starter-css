471ce6fe7f7638cb05d42c39cd317c1494f8a7bd
Mr. Genius <70088433+Muzammil2008@users.noreply.github.com>
2020-10-22 20:33:28 +0530

    Update index.js

--- birthday 1/Birthday/js/index.js
+++ birthday 1/Birthday/js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube-nocookie.com/embed/7vQsBI6C8Qk?autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/7vQsBI6C8Qk?autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
